5f01605f00ff5ae5d7f397a33e3f1ef768539fe8
akmal <akmalkarimboyev50@gmail.com>
2026-03-28 15:45:38 +0500

    Refactor A0001.py to improve input handling and add additional calculations

--- A0001.py
+++ A0001.py
@@ -3,3 +3,20 @@ b = int(input())
 
 yigindi = a + b
 print(yigindi)
+
+#yosh topish
+tugilgan_yil = int(input())
+joriy_yil = 2024
+yosh = joriy_yil - tugilgan_yil
+print(yosh)
+
+#kvadratni topish
+son = int(input())
+kvadrat = son ** 2
+print(kvadrat)
+
+#yoshni topish
+tugilgan_yil = int(input())         
+joriy_yil = 2024
+yosh = joriy_yil - tugilgan_yil
+print(yosh)
